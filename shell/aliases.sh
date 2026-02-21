bbfbb87131ed7447ccceac94f3da38923cda6a44
Peter Siska <psiska@duplex.local>
2026-02-21 13:04:31 +0100

    chore: add practical shell aliases

--- shell/aliases.sh
+++ shell/aliases.sh
@@ -1,1 +1,11 @@
 # Shared aliases for zsh and bash.
+
+# Easier navigation
+alias ..="cd .."
+alias ...="cd ../.."
+alias ....="cd ../../.."
+alias .....="cd ../../../.."
+alias -- -="cd -"
+
+# macOS/BSD ls with colors
+alias l="ls -AlFh -G"
