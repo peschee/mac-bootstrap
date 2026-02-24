66bfaab000e46da30dc08f2b66c3bedd3ce9e8d5
Peter Siska <63866+peschee@users.noreply.github.com>
2026-02-25 00:35:23 +0100

    Add "resource" alias to re-source zsh config

--- shell/aliases.sh
+++ shell/aliases.sh
@@ -11,5 +11,8 @@ alias j="z"
 # macOS/BSD ls with colors
 alias l="ls -AlFh -G"
 
+# Re-source zsh config
+alias resource="source ~/.zshrc"
+
 # System update
 alias update="sudo softwareupdate -i -a; brew update; brew upgrade; brew cleanup"
